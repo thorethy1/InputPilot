840b99d4d1232a5a23e30f0a8f93d8deb3f1d93e
thorethy <191354889+thorethy1@users.noreply.github.com>
2026-08-26 07:30:39 +0200

    chore(android): align version with 0.6.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.5.0"
+        versionName = "0.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
